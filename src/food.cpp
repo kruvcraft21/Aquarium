1d38077264ffd927ba0b50b89196ec751493654d
kruvcraft21 <kruvcraft@yandex.ru>
2023-04-17 18:29:59 +0500

    New fields added

--- src/food.cpp
+++ src/food.cpp
@@ -6,20 +6,31 @@ Food::Food(Vector2 pos)
     this->Draw();
 }
 
-bool Food::is_eaten(Fish *fishes)
+bool Food::is_eaten(Fish *fish)
 {
     bool eaten = false;
     for (int i = 0; i < MAX_FISH; i++)
     {
-        if (CheckCollisionPointCircle(fishes[i].get_Coord(), this->Coord, 5)) {
-            eaten |= true;
-            fishes[i].eats(1);
+        if (CheckCollisionPointCircle(fish[i].get_Coord(), this->Coord, radius))
+        {
+            eaten = true;
+            fish[i].eats(1);
         }
     }
     return eaten;
 }
 
+int Food::get_radius()
+{
+    return this->radius;
+}
+
+Color Food::get_color()
+{
+    return this->color;
+}
+
 void Food::Draw()
 {
-    DrawCircleV(Coord, 5, YELLOW);
+    DrawCircleV(Coord, radius, color);
 }
