0f6280f47f3fd5146759f0af4e3ef6a12168e63a
kruvcraft21 <kruvcraft@yandex.ru>
2023-04-17 23:04:24 +0500

    Adding comments `aquarium.cpp` and `fish.cpp`

--- src/aquarium.cpp
+++ src/aquarium.cpp
@@ -1,31 +1,48 @@
 ﻿#include "aquarium.h"
 
+// Конструктор класса Aquarium
 Aquarium::Aquarium(){
+    // Устанавливаем высоту аквариума равную заданному значению AQUARIUM_HEIGHT
     this->height = AQUARIUM_HEIGHT;
+    // Устанавливаем ширину аквариума равную заданному значению AQUARIUM_WIDTH 
     this->width = AQUARIUM_WIDTH;
 }
 
+// Метод инициализации аквариума
 void Aquarium::Init() {
-    InitWindow(this->width, this->height, "Aquarium");
-    SetTargetFPS(30);
+    // Инициализация окна с использованием установленных значений высоты и ширины аквариума, а также заголовка окна "Aquarium"
+    InitWindow(this->width, this->height, "Aquarium"); 
+    // Устанавливаем целевое количество кадров в секунду (FPS) на 30
+    SetTargetFPS(30); 
+    // Создаем уникальный указатель на массив объектов типа Fish, выделяем память на максимальное количество рыб MAX_FISH
     this->fish = std::make_unique<Fish[]>(MAX_FISH);
+    // Создаем уникальный указатель на массив объектов типа Rock, выделяем память на максимальное количество скал MAX_ROCK
     this->rock = std::make_unique<Rock[]>(MAX_ROCK);
-    this->Run();
+    // Запускаем основной цикл выполнения программы в методе Run()
+    this->Run(); 
 }
 
+// Метод запуска цикла обновления и отрисовки аквариума
 void Aquarium::Run() {
+    // Цикл выполняется до тех пор, пока окно не будет закрыто
     while (!WindowShouldClose()) {
+        // Начало отрисовки кадра
         BeginDrawing();
+            // Очистка фона окна, устанавливая его цвет на светло-голубой
             ClearBackground(SKYBLUE);
+            // Цикл для инициализации всех скал в аквариуме
             for (int i = 0; i < MAX_ROCK; i ++) {
                 rock[i].Init();
             }
             for (int i = 0; i < MAX_FISH; i++) {
+                // Инициализация объектов рыбы
                 this->fish[i].Init();
+                // Выполнение движения рыбы и взаимодействия с объектами скалы
                 this->fish[i].Run(this->rock.get());
             }
-
+            // Отрисовка текущего FPS на экране
             DrawFPS(10, 10);
+        // Завершение рисования на экране
         EndDrawing();
     }
 }
