bd6715576fcf2b77246870af0d44cb71cabf8fe7
kruvcraft21 <kruvcraft@yandex.ru>
2023-04-17 21:19:11 +0500

    Minor changes and adding comments

--- src/rock.cpp
+++ src/rock.cpp
@@ -1,30 +1,45 @@
 ﻿#include "rock.h"
 #include <math.h>
 
+// Конструктор по умолчанию
 Rock::Rock()
 {
-    int posy = GetRandomValue(ZONA_ROCK_MIN, ZONA_ROCK_MAX);
-    int posx = GetRandomValue(0, AQUARIUM_WIDTH);
-    Coord = {(float)posx, (float)posy};
-    size = AQUARIUM_HEIGTH - posy;
-    colorbody = GRAY;
-    offset_side1 = {(float)GetRandomValue(0, size), (float)GetRandomValue(ZONA_ROCK_MIN - posy, size)};
-    offset_side2 = {(float)GetRandomValue(0, size), (float)size};
-    offset_side3 = {(float)GetRandomValue(0, size), (float)size};
-    offset_side4 = {(float)GetRandomValue(0, size), (float)GetRandomValue(ZONA_ROCK_MIN - posy, size)};
+    // Инициализация координаты и размера объекта
+    int posy = GetRandomValue(ZONA_ROCK_MIN, ZONA_ROCK_MAX); // Случайная координата Y в заданном диапазоне
+    int posx = GetRandomValue(0, AQUARIUM_WIDTH); // Случайная координата X в диапазоне ширины аквариума
+    Coord = {(float)posx, (float)posy}; // Установка координаты объекта
+    size = AQUARIUM_HEIGTH - posy; // Вычисление размера объекта на основе его Y-координаты
+
+    // Инициализация цвета и смещений точек объекта
+    colorbody = GRAY; // Установка цвета тела объекта
+    offset_side1 = {(float)GetRandomValue(0, size), (float)GetRandomValue(ZONA_ROCK_MIN - posy, size)}; // Смещение точки 1
+    offset_side2 = {(float)GetRandomValue(0, size), (float)size}; // Смещение точки 2
+    offset_side3 = {(float)GetRandomValue(0, size), (float)size}; // Смещение точки 3
+    offset_side4 = {(float)GetRandomValue(0, size), (float)GetRandomValue(ZONA_ROCK_MIN - posy, size)}; // Смещение точки 4
 }
 
+// Метод инициализации объекта
 void Rock::Init()
 {
-    float pos_x = Coord.x, pos_y = Coord.y;
-    pfd[0] = {pos_x - offset_side1.x, pos_y + offset_side1.y};
-    pfd[1] = {pos_x - offset_side2.x, pos_y + offset_side2.y};
-    pfd[2] = {pos_x + offset_side3.x, pos_y + offset_side3.y};
-    pfd[3] = {pos_x + offset_side4.x, pos_y + offset_side4.y};
-    Draw();
+    float pos_x = Coord.x; // Координата X объекта
+    float pos_y = Coord.y; // Координата Y объекта
+
+    // Вычисление координат точек на основе смещений
+    pfd[0] = {pos_x - offset_side1.x, pos_y + offset_side1.y}; // Координаты первой точки
+    pfd[1] = {pos_x - offset_side2.x, pos_y + offset_side2.y}; // Координаты второй точки
+    pfd[2] = {pos_x + offset_side3.x, pos_y + offset_side3.y}; // Координаты третьей точки
+    pfd[3] = {pos_x + offset_side4.x, pos_y + offset_side4.y}; // Координаты четвертой точки
+
+    Draw(); // Вызов функции отрисовки
 }
 
+// Метод получения указателя на массив точек для отрисовки
 Vector2* Rock::get_pfd()
 {
     return pfd.get();
+}
+
+// Метод получения цвета тела объекта
+Color Rock::get_colorbody() {
+    return this->colorbody;
 }
--- src/rock.h
+++ src/rock.h
@@ -6,14 +6,14 @@
 class Rock : Entity
 {
 private:
-    Vector2 offset_side1 = {0, 0};
-    Vector2 offset_side2 = {0, 0};
-    Vector2 offset_side3 = {0, 0};
-    Vector2 offset_side4 = {0, 0};
+    Vector2 offset_side1; // Смещение для первой точки объекта
+    Vector2 offset_side2; // Смещение для второй точки объекта
+    Vector2 offset_side3; // Смещение для третьей точки объекта
+    Vector2 offset_side4; // Смещение для четвертой точки объекта
 
 public:
-    Rock();
-    void Init();
-    Vector2 *get_pfd();
-    Color get_colorbody() { return this->colorbody; }
+    Rock(); // Конструктор по умолчанию
+    void Init() override; // Метод инициализации объекта
+    Vector2 *get_pfd(); // Метод получения указателя на массив точек для отрисовки
+    Color get_colorbody(); // Метод получения цвета тела объекта
 };
