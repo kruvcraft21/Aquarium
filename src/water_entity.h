f79a89c43635532560804129eafb826c9b36f8fc
kruvcraft21 <kruvcraft@yandex.ru>
2023-04-17 21:30:29 +0500

    Correcting inaccuracies and adding comments

--- src/water_entity.h
+++ src/water_entity.h
@@ -4,23 +4,23 @@
 #include <memory>
 #include <vector>
 
-#define AQUARIUM_HEIGHT 450
-#define AQUARIUM_WIDTH 800
-#define MAX_POINTS 4
-#define ZONA_ROCK_MIN AQUARIUM_HEIGHT - (AQUARIUM_HEIGHT / 5)
-#define ZONA_ROCK_MAX AQUARIUM_HEIGHT - 40
-#define MAX_FISH 10
-#define MAX_ROCK 10
+constexpr int AQUARIUM_HEIGHT = 450; // Высота аквариума
+constexpr int AQUARIUM_WIDTH = 800; // Ширина аквариума
+constexpr int MAX_POINTS = 4; // Максимальное количество точек для отрисовки
+constexpr int ZONA_ROCK_MIN = AQUARIUM_HEIGHT - (AQUARIUM_HEIGHT / 5); // Минимальная высота для расположения объекта "скала" в зоне
+constexpr int ZONA_ROCK_MAX = AQUARIUM_HEIGHT - 40; // Максимальная высота для расположения объекта "скала" в зоне
+constexpr int MAX_FISH = 10; // Максимальное количество объектов "рыба"
+constexpr int MAX_ROCK = 10; // Максимальное количество объектов "скала"
 
 class Entity
 {
 protected:
-    std::unique_ptr<Vector2[]> pfd = std::make_unique<Vector2[]>(MAX_POINTS); // points for draw
-    Color colorbody = BLACK;
-    int size = 10;
-    Vector2 Coord = {AQUARIUM_WIDTH / 2, AQUARIUM_HEIGHT / 2};
+    std::unique_ptr<Vector2[]> pfd = std::make_unique<Vector2[]>(MAX_POINTS); // Массив точек для отрисовки
+    Color colorbody = BLACK; // Цвет тела объекта
+    int size = 10; // Размер объекта
+    Vector2 Coord = {AQUARIUM_WIDTH / 2, AQUARIUM_HEIGHT / 2}; // Координаты объекта
 
 public:
-    virtual void Init() = 0;
-    void Draw();
+    virtual void Init() = 0; // Виртуальный метод инициализации объекта
+    void Draw(); // Метод отрисовки объекта
 };
